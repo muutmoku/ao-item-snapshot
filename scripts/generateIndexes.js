0b06e4a06bd831f88272449a4a999e9ec507e9a4
Muut Moku <mokumoku.ukabu@gmail.com>
2025-05-20 21:30:27 +0900

    add index pages

---
+++ scripts/generateIndexes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const toHTML = require('directory-index-html');
+
+function generateIndex(dirPath) {
+  const entries = fs.readdirSync(dirPath).map(name => {
+    const fullPath = path.join(dirPath, name);
+    const stats = fs.statSync(fullPath);
+    return {
+      name: stats.isDirectory() ? `${name}/` : name,
+      size: stats.size,
+      mtime: stats.mtime
+    };
+  });
+
+  const html = toHTML(dirPath, entries);
+  fs.writeFileSync(path.join(dirPath, 'index.html'), html);
+}
+
+function walkDir(currentPath) {
+  generateIndex(currentPath);
+  fs.readdirSync(currentPath).forEach(name => {
+    const fullPath = path.join(currentPath, name);
+    if (fs.statSync(fullPath).isDirectory()) {
+      walkDir(fullPath);
+    }
+  });
+}
+
+walkDir(path.join(__dirname, '..', 'docs', 'data'));
